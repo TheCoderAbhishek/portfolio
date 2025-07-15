54ed1c2f1af1b2456d49df10c9b9e89c29bcd753
Abhishek Patil <patilabhishek394@gmail.com>
2025-07-15 14:03:49 +0530

    Experience section for the portfolio created and designed.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
+import { Experience } from './pages/experience/experience';
 
 export const routes: Routes = [
   // When the user is at the base URL, show the HomeComponent
   { path: '', component: Home, pathMatch: 'full' },
   { path: 'about', component: About },
+  { path: 'experience', component: Experience },
   // You can add other routes here later
   // { path: 'about', component: AboutComponent },
   // { path: 'projects', component: ProjectsComponent },
