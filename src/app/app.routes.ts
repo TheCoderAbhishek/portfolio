03fca5c4000ae8e20993ca19a04ee123a9330bd6
Abhishek Patil <patilabhishek394@gmail.com>
2025-07-15 10:43:56 +0530

    Create home component with all the required details.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // When the user is at the base URL, show the HomeComponent
+  { path: '', component: Home, pathMatch: 'full' },
+
+  // You can add other routes here later
+  // { path: 'about', component: AboutComponent },
+  // { path: 'projects', component: ProjectsComponent },
+];
