6c7a56dfae9d74e4dccb000363ae8f90c142d6b1
Abhishek Patil <patilabhishek394@gmail.com>
2025-07-15 13:52:56 +0530

    Design and implement About component for the portfolio.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
 
 export const routes: Routes = [
   // When the user is at the base URL, show the HomeComponent
   { path: '', component: Home, pathMatch: 'full' },
-
+  { path: 'about', component: About },
   // You can add other routes here later
   // { path: 'about', component: AboutComponent },
   // { path: 'projects', component: ProjectsComponent },
